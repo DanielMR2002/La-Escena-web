42e448f48554ee2c4da5cd4b5378b09b687a9ef5
DanielMR2002 <daniel.medina.r18@gmail.com>
2026-01-29 15:01:43 -0500

    dep, deploy

--- la-escena-cms/sanity.cli.ts
+++ la-escena-cms/sanity.cli.ts
@@ -6,6 +6,7 @@ export default defineCliConfig({
     dataset: 'production'
   },
   deployment: {
+    appId: 'p12cgd8x1ykkwfkaxtbgeh3k',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/cli#auto-updates
