8459a8acc9916063657825d1aae9fee017d9db67
DanielMR2002 <daniel.medina.r18@gmail.com>
2026-02-03 01:37:29 -0500

    sitemap

---
+++ la-escena-web/app/sitemap.ts
@@ -0,0 +1,56 @@
+import { MetadataRoute } from 'next'
+import { getPosts, getArtists } from '@/lib/sanity'
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const baseUrl = 'https://la-escena-web.vercel.app' 
+
+  // 📝 Blog
+  const posts = await getPosts()
+
+  const blogUrls = posts.map((post: any) => ({
+    url: `${baseUrl}/blog/${post.slug.current}`,
+    lastModified: post.publishedAt
+      ? new Date(post.publishedAt)
+      : new Date(),
+  }))
+
+  // 🎭 Artistas
+  const artists = await getArtists()
+
+  const artistUrls = artists.map((artist: any) => ({
+    url: `${baseUrl}/artistas/${artist.slug.current}`,
+    lastModified: new Date(),
+  }))
+
+  return [
+    //  Páginas estáticas
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/agencia`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/clases`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/contacto`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/blog`,
+      lastModified: new Date(),
+    },
+
+    // 🔁 Dinámicas
+    ...blogUrls,
+    ...artistUrls,
+  ]
+}
