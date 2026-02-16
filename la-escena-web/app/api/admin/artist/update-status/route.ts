ba0e955a056e06ae835401c78538a9be9ff9cd8e
DanielMR2002 <daniel.medina.r18@gmail.com>
2026-02-15 20:57:00 -0500

    fix vercel

--- la-escena-web/app/api/admin/artist/update-status/route.ts
+++ la-escena-web/app/api/admin/artist/update-status/route.ts
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic"
+
+
 import { getServerSession } from "next-auth"
 import { authOptions } from "@/api/auth/[...nextauth]/route"
 import { NextResponse } from "next/server"
